e3f082b87072278260adc7ad37d2d94a99e03c0e
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-27 16:20:26 +0300

    Added filter to webhook

--- Apps.Synthesia/Webhooks/VideoWebhookList.cs
+++ Apps.Synthesia/Webhooks/VideoWebhookList.cs
@@ -1,4 +1,5 @@
-﻿using Apps.Synthesia.Webhooks.Base.Handlers;
+﻿using Apps.Synthesia.Models;
+using Apps.Synthesia.Webhooks.Base.Handlers;
 using Apps.Synthesia.Webhooks.Models;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Invocation;
@@ -11,11 +12,23 @@ namespace Apps.Synthesia.Webhooks
     public class VideoWebhookList(InvocationContext invocationContext) : BaseInvocable(invocationContext)
     {
         [Webhook("On video completed", typeof(VideoCompletedHandler), Description = "On any video completed")]
-        public Task<WebhookResponse<VideoCompletedPayload>> OnVideoCompleted(WebhookRequest webhookRequest)
+        public Task<WebhookResponse<VideoCompletedPayload>> OnVideoCompleted(WebhookRequest webhookRequest,
+            [WebhookParameter] VideoOptionFilter input)
         {
             var payload = webhookRequest.Body.ToString();
             ArgumentException.ThrowIfNullOrEmpty(payload, nameof(webhookRequest.Body));
             var result = JsonConvert.DeserializeObject<VideoCompletedPayload>(payload);
+
+            if (!string.IsNullOrWhiteSpace(input?.VideoId)
+        && !string.Equals(result.Data.Id, input.VideoId, StringComparison.OrdinalIgnoreCase))
+            {
+                return Task.FromResult(new WebhookResponse<VideoCompletedPayload>
+                {
+                    HttpResponseMessage = null,
+                    Result = null!,
+                    ReceivedWebhookRequestType = WebhookRequestType.Default
+                });
+            }
 
             return Task.FromResult(new WebhookResponse<VideoCompletedPayload>
             {
