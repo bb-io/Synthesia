0b54864c930249918b1a6f843bcf9c7d662f72f1
RiabushenkoA <ariabushenko@blackbird.io>
2025-05-28 12:20:20 +0300

    Added after subscription to 'On  video completed' event

---
+++ Apps.Synthesia/Models/VideoDto.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Apps.Synthesia.Models
+{
+    public class VideoDto
+    {
+        [JsonProperty("captions")]
+        public Captions Captions { get; set; }
+
+        [JsonProperty("createdAt")]
+        public long CreatedAt { get; set; }
+
+        [JsonProperty("ctaSettings")]
+        public CtaSettings CtaSettings { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("download")]
+        public string Download { get; set; }
+
+        [JsonProperty("duration")]
+        public string Duration { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("lastUpdatedAt")]
+        public long LastUpdatedAt { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("thumbnail")]
+        public Thumbnail Thumbnail { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("visibility")]
+        public string Visibility { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        [JsonProperty("image")]
+        public string Image { get; set; }
+
+        [JsonProperty("gif")]
+        public string Gif { get; set; }
+    }
+}
--- Apps.Synthesia/Webhooks/Base/Handlers/VideoCompletedHandler.cs
+++ Apps.Synthesia/Webhooks/Base/Handlers/VideoCompletedHandler.cs
@@ -1,13 +1,34 @@
-﻿using Blackbird.Applications.Sdk.Common.Invocation;
+﻿using Apps.Synthesia.Api;
+using Apps.Synthesia.Models;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Blackbird.Applications.Sdk.Common.Webhooks;
+using RestSharp;
 
 namespace Apps.Synthesia.Webhooks.Base.Handlers
 {
-    public class VideoCompletedHandler : SynthesiaWebhookHandler
+    public class VideoCompletedHandler(InvocationContext invocationContext,[WebhookParameter] VideoOptionFilter input)
+     : SynthesiaWebhookHandler(invocationContext), IAfterSubscriptionWebhookEventHandler<VideoDto>
     {
-        public VideoCompletedHandler(InvocationContext invocationContext) : base(invocationContext)
-        {
-        }
-
         protected override List<string> SubscriptionEvents => new List<string> { "video.completed" };
+
+        public async Task<AfterSubscriptionEventResponse<VideoDto>> OnWebhookSubscribedAsync()
+        {
+            if (string.IsNullOrWhiteSpace(input.VideoId))
+                return null;
+
+            var client = new SynthesiaClient(InvocationContext.AuthenticationCredentialsProviders);
+            var request = new RestRequest($"/webhooks/{input}", Method.Get);
+            var video = await client.ExecuteWithErrorHandling<VideoDto>(request);
+
+            if (video.Status.Equals("complete", StringComparison.OrdinalIgnoreCase))
+            {
+                return new AfterSubscriptionEventResponse<VideoDto>
+                {
+                    Result = video
+                };
+            }
+
+            return null;
+        }
     }
 }
